09a8694009e574798e43766f7526212e57376ba2
Juan Marcelo Tisot <marcelotisot1990@gmail.com>
2024-10-10 21:01:00 -0300

    se instala el paquete uuid para manejar los ids, se agrega CreateCarDto

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, ParseUUIDPipe, Patch, Post } from '@nestjs/common';
 import { CarsService } from './cars.service';
+import { CreateCarDto } from './dto/create-car.dto';
 
 @Controller('cars')
 export class CarsController {
@@ -14,8 +15,28 @@ export class CarsController {
   }
 
   @Get(':id')
-  getCarById( @Param('id', ParseIntPipe ) id: number ) {
+  getCarById( @Param('id', ParseUUIDPipe) id: string ) {
     return this.carsService.findOneById( id );
+  }
+
+  @Post()
+  createCar( @Body() createCarDto: CreateCarDto ) {
+    return createCarDto;
+  }
+
+  @Patch(':id')
+  updateCar( @Param('id', ParseIntPipe) id: number,
+  @Body() body: any ) 
+  {
+    return body;
+  }
+
+  @Delete(':id')
+  deleteCar( @Param('id', ParseIntPipe) id: number ) {
+    return {
+      method: 'DELETE',
+      id: id
+    }
   }
 
 }
